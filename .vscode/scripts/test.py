2b6ad017b03247d8b483dfb92519308e90092428
Mustafa <shaukat.mustafa01@gmail.com>
2025-12-14 15:45:20 +0000

    asd

--- .vscode/scripts/test.py
+++ .vscode/scripts/test.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
 
-
 data = {
     "Name": ["Ali", "Sara", "John"],
     "Age": [25, 30, 28],
     "City": ["Lahore", "Karachi", "New York"]
 }
 
-
-
 df = pd.DataFrame(data)
 
 print(df)
+
+
+
+
+
+
+"python test.py"
+"python -m pip install pandas"
