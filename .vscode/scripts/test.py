f23a0d91df3dd18866a46c28f35ab0af599fa253
Mustafa <shaukat.mustafa01@gmail.com>
2025-12-14 15:35:12 +0000

    asdf

--- .vscode/scripts/test.py
+++ .vscode/scripts/test.py
@@ -1,2 +1,15 @@
-import pd
+import pandas as pd
 
+
+
+data = {
+    "Name": ["Ali", "Sara", "John"],
+    "Age": [25, 30, 28],
+    "City": ["Lahore", "Karachi", "New York"]
+}
+
+
+
+df = pd.DataFrame(data)
+
+print(df)
